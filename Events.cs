a0fe01f7dc41786b45bed71902301d476b36b46a
Tim R <trichards77@gmail.com>
2023-05-29 14:33:25 -0700

    Initial commit

---
+++ Events.cs
@@ -0,0 +1,127 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RunCliCommandOnSave {
+  internal class Events : IVsRunningDocTableEvents3 {
+    private readonly DTE _dte;
+    private readonly RunningDocumentTable _runningDocumentTable;
+    private OutputWindowPane _outputPane;
+    private const string kAppName = "CLI On Save";
+
+    public Events(DTE dte, RunningDocumentTable runningDocumentTable) {
+      _runningDocumentTable = runningDocumentTable;
+      _dte = dte;
+    }
+
+    public int OnBeforeSave(uint docCookie) {
+      ThreadHelper.ThrowIfNotOnUIThread();
+      Process(docCookie, "PreSave");
+      return VSConstants.S_OK;
+    }
+
+    public int OnAfterSave(uint docCookie) {
+      ThreadHelper.ThrowIfNotOnUIThread();
+      Process(docCookie, "PostSave");
+      return VSConstants.S_OK;
+    }
+
+    public int OnAfterFirstDocumentLock(uint DocCookie, uint dwRdtLockType, uint dwReadLocksRemaining, uint dwEditLocksRemaining) { return VSConstants.S_OK; }
+    public int OnBeforeLastDocumentUnlock(uint DocCookie, uint dwRdtLockType, uint dwReadLocksRemaining, uint dwEditLocksRemaining) { return VSConstants.S_OK; }
+    public int OnAfterAttributeChange(uint DocCookie, uint grfAttribs) { return VSConstants.S_OK; }
+    public int OnBeforeDocumentWindowShow(uint DocCookie, int fFirstShow, IVsWindowFrame pFrame) { return VSConstants.S_OK; }
+    public int OnAfterDocumentWindowHide(uint DocCookie, IVsWindowFrame pFrame) { return VSConstants.S_OK; }
+    int IVsRunningDocTableEvents3.OnAfterAttributeChangeEx(uint DocCookie, uint grfAttribs, IVsHierarchy pHierOld, uint itemidOld, string pszMkDocumentOld, IVsHierarchy pHierNew, uint itemidNew, string pszMkDocumentNew) { return VSConstants.S_OK; }
+    int IVsRunningDocTableEvents2.OnAfterAttributeChangeEx(uint DocCookie, uint grfAttribs, IVsHierarchy pHierOld, uint itemidOld, string pszMkDocumentOld, IVsHierarchy pHierNew, uint itemidNew, string pszMkDocumentNew) { return VSConstants.S_OK; }
+
+    private void Process(uint docCookie, string section) {
+      ThreadHelper.ThrowIfNotOnUIThread();
+      var documents = _dte.Documents.Cast<Document>();
+      var documentToFormat = CookieToDoc(docCookie, documents);
+      if (documentToFormat != null) {
+        if (_dte.ActiveWindow.Kind != "Document") return;
+        var ActiveDocument = _dte.ActiveDocument;
+        var FileSettings = new Settings(documentToFormat.FullName);
+        if (FileSettings == null) return;
+        var commands = FileSettings.GetCommand(ActiveDocument.FullName, section);
+        var config = FileSettings.GetConfig(_dte);
+
+        List<string> errors = new List<string>();
+        if (commands != null) {
+          var tokens = FileSettings.GetTokens(_dte);
+          documentToFormat.Activate();
+          foreach ((string _cmd, string _args) in commands) {
+            var cmd = _cmd.Replace(tokens.file.key, tokens.file.value).
+              Replace(tokens.fileName.key, tokens.fileName.value).
+              Replace(tokens.filePath.key, tokens.filePath.value).
+              Replace(tokens.solutionDir.key, tokens.solutionDir.value);
+            var args = _args.Replace(tokens.file.key, tokens.file.value).
+              Replace(tokens.fileName.key, tokens.fileName.value).
+              Replace(tokens.filePath.key, tokens.filePath.value).
+              Replace(tokens.solutionDir.key, tokens.solutionDir.value);
+            try {
+              var process = new System.Diagnostics.Process {
+                StartInfo = new System.Diagnostics.ProcessStartInfo {
+                  FileName = cmd,
+                  Arguments = args,
+                  WorkingDirectory = config.workingDir,
+                }
+              };
+              process.Start();
+            } catch (Exception e) {
+              errors.Add($"Command: {_cmd} Args {_args} Exception: {e.Message}");
+            }
+          }
+          ActiveDocument.Activate();
+        }
+
+        if (config.debug) {
+          if (commands == null || commands.Count == 0 || errors.Count > 0) {
+            Log($"{documentToFormat.FullName} Error At {section}\n");
+            foreach (var error in errors) {
+              Log($"\tError:{error}\n");
+            }
+          } else {
+            Log(String.Format($"Processed {documentToFormat.FullName} section {section}\n"));
+          }
+        }
+      }
+    }
+
+    private void Log(string message) {
+      ThreadHelper.ThrowIfNotOnUIThread();
+      var dte2 = _dte as DTE2;
+      if (dte2 != null) {
+        var panes = dte2.ToolWindows.OutputWindow.OutputWindowPanes;
+        try {
+          _outputPane = panes.Item(kAppName);
+        } catch (ArgumentException) {
+        }
+
+        if (_outputPane == null) {
+          _outputPane = panes.Add(kAppName);
+        }
+
+        if (_outputPane != null) {
+          _outputPane.OutputString(message);
+        }
+      }
+    }
+
+    private Document CookieToDoc(uint docCookie, IEnumerable<Document> documents) {
+      ThreadHelper.ThrowIfNotOnUIThread();
+      foreach (var Doc in documents) {
+        if (Doc.FullName == _runningDocumentTable.GetDocumentInfo(docCookie).Moniker) {
+          return Doc;
+        }
+      }
+      return null;
+    }
+
+  }
+}
---
+++ Settings.cs
@@ -0,0 +1,136 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RunCliCommandOnSave {
+  public class Settings {
+    public Settings(string docFullName) {
+      _settingsFilename = LocateSettings(Path.GetDirectoryName(docFullName));
+    }
+
+    public string ReadKey(string section, string key) {
+      var Temp = new StringBuilder(256);
+      var NumberOfChars = GetPrivateProfileString(section, key, null, Temp, Temp.Capacity, _settingsFilename);
+      return NumberOfChars == 0 ? null : Temp.ToString();
+    }
+
+    internal ((string key, string value) file, (string key, string value) fileName, (string key, string value) filePath, (string key, string value) solutionDir)
+      GetTokens(DTE dte) {
+      ThreadHelper.ThrowIfNotOnUIThread();
+      var activeDocument = dte.ActiveDocument;
+      return (file: ("{File}", activeDocument.FullName),
+        fileName: ("{FileName}", activeDocument.Name),
+        filePath: ("{FilePath}", activeDocument.Path),
+        solutionDir: ("{SolutionDir}",
+          System.IO.Path.GetDirectoryName(dte.Solution.FullName)));
+    }
+
+    internal string ResolveTokens(DTE dte, string input) {
+      ThreadHelper.ThrowIfNotOnUIThread();
+      var tokens = GetTokens(dte);
+      return input.Replace(tokens.file.key, tokens.file.value).
+      Replace(tokens.fileName.key, tokens.fileName.value).
+      Replace(tokens.filePath.key, tokens.filePath.value).
+      Replace(tokens.solutionDir.key, tokens.solutionDir.value);
+    }
+
+    internal (string workingDir, bool debug) GetConfig(DTE dte) {
+      ThreadHelper.ThrowIfNotOnUIThread();
+      var WDRaw = ReadKey("Config", "WorkingDir");
+      var debugRaw = ReadKey("Config", "Debug");
+      string workingDir = WDRaw == null ? System.IO.Path.GetDirectoryName(dte.Solution.FullName) : WDRaw;
+      workingDir = this.ResolveTokens(dte, workingDir);
+      bool debug = false;
+      if (debugRaw != null) {
+        bool.TryParse(debugRaw.Trim().ToLower(), out debug);
+      }
+      return (workingDir: workingDir, debug: debug);
+    }
+
+    public List<(string cmd, string args)> GetCommand(string docFullName, string action) {
+      var result = new List<(string cmd, string args)>();
+      var CommandsRaw = ReadKey(action, "Commands");
+      if (CommandsRaw == null) {
+        return null;
+      }
+      // Handles escaped pipe
+      const string kPipe = "~<PIPE>~";
+      CommandsRaw = CommandsRaw.Replace("^|", kPipe);
+      var Commands = CommandsRaw.Split(new char[] { '|' }, System.StringSplitOptions.RemoveEmptyEntries);
+      if (Commands.Length == 0) {
+        return null;
+      }
+      foreach (var c in Commands) {
+        var command = c.Replace(kPipe, "|");
+
+        string cmdPattern = @"cmd=(?<cmd>[^\s]+)";
+        string argsPattern = @"args=(?<args>.*)";
+
+        Match cmdMatch = Regex.Match(command, cmdPattern);
+        Match argsMatch = Regex.Match(command, argsPattern);
+
+        if (cmdMatch.Success && argsMatch.Success) {
+          string cmd = cmdMatch.Groups["cmd"].Value;
+          string args = argsMatch.Groups["args"].Value;
+
+          result.Add((cmd: cmd, args: args));
+        }
+      }
+
+      // Check excluded paths (they can be relative or absolute separated by |)
+      var ExcludedPathsRaw = ReadKey(action, "ExcludePaths");
+      if (ExcludedPathsRaw != null) {
+        var BasePath = Path.GetDirectoryName(_settingsFilename);
+        var ExcludedPathsRawList = ExcludedPathsRaw.Split(new char[] { '|' }, System.StringSplitOptions.RemoveEmptyEntries);
+        var ExcludedPaths = ExcludedPathsRawList.Select(s => new FileInfo(Path.Combine(BasePath, s.Replace("/", "\\").Replace("\"", ""))).FullName);
+        try {
+          foreach (var Path in ExcludedPaths) {
+            if (docFullName.StartsWith(Path)) {
+              return null;
+            }
+          }
+        } catch (Exception) {
+        }
+      }
+
+      // Check include extensions (separated by |)
+      var IncludeExtensionsRaw = ReadKey(action, "IncludeExtensions");
+      if (IncludeExtensionsRaw != null) {
+        var IncludeExtensions = IncludeExtensionsRaw.Split(new char[] { '|' }, System.StringSplitOptions.RemoveEmptyEntries).Select(s => s.TrimSuffix("*"));
+        try {
+          if (!IncludeExtensions.Any(ext => { return docFullName.EndsWith(ext); })) {
+            return null;
+          }
+        } catch (Exception) {
+        }
+      }
+
+      return result;
+    }
+
+    private string _settingsFilename = null;
+
+    private string LocateSettings(string startingPath) {
+      var cfgFile = new FileInfo(Path.Combine(startingPath, ".run-cli-on-save"));
+      var dir = new DirectoryInfo(startingPath);
+      while (!cfgFile.Exists && dir.Parent != null) {
+        var configs = dir.GetFiles(".run-cli-on-save");
+        if (configs.Length > 0) {
+          cfgFile = configs[0];
+          break;
+        }
+        dir = dir.Parent;
+      }
+      return cfgFile.Exists ? cfgFile.FullName : null;
+    }
+
+    [DllImport("kernel32")]
+    private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+  }
+}
